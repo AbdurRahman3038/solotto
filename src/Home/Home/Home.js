2af892c068e62c7ca93a08193560a7d44a71a457
AbdurRahman3038 <abdurrahman.iubat@gmail.com>
2022-01-18 02:43:49 +0600

    icon added

--- src/Home/Home/Home.js
+++ src/Home/Home/Home.js
@@ -11,19 +11,9 @@ import discord from '../../images/Discord Twitter.png';
 
 const Home = () => {
     return (
-        <div className="container">
+        <div className="main-container">
 
-            {/* <Box className='discord-logo' sx={{ display: 'flex', justifyContent: 'end', }}>
-                <Box className='discord' sx={{ position: 'fixed', width: '280px', marginRight: "50px" }}>
-                    <img src={discord} alt="" />
-                </Box>
-                <Box sx={{ pt: 10 }}>
-                    <img className="logo" style={{ width: '510px', height: '153px', }} src={logo} alt="" />
-                </Box>
-
-            </Box> */}
-
-            <Container>
+            <Container className='container'>
 
                 <div className='header'>
                     <img className='logo-img' src={logo} alt="" />
@@ -34,9 +24,9 @@ const Home = () => {
                 <Box className='header-section' sx={{ display: 'flex', justifyContent: 'center', mt: { md: '100px' } }}>
                     <Box className="header-container" sx={{ borderRadius: '20px', p: 10, width: { xs: '100%', md: '900px' } }}>
 
-                        <Typography className='join-us' sx={{ color: 'white', mr: '20px', fontSize: "70px" }} variant="h3">JOIN US TODAY</Typography>
+                        <Typography className='join-us font-style' sx={{ color: 'white', mr: '20px', fontSize: "70px" }} variant="h3">JOIN US TODAY</Typography>
 
-                        <Typography className='join-us' sx={{ color: 'white', textAlign: 'center', pt: 2, pl: { md: '115px' }, fontWeight: 600, fontSize: "70px" }} variant="h3">BIG WIN WITH US</Typography>
+                        <Typography className='join-us font-style' sx={{ color: 'white', textAlign: 'center', pt: 2, pl: { md: '115px' }, fontWeight: 600, fontSize: "70px" }} variant="h3">BIG WIN WITH US</Typography>
 
                         <Typography sx={{ textAlign: 'center', mt: 4, typography: { xs: 'h4', sm: 'h4', md: 'h1' } }} variant="h1" className="soloto">SOLOTTO</Typography>
                     </Box>
@@ -48,7 +38,7 @@ const Home = () => {
 
                         <Typography className="soloto what-soloto" sx={{ color: 'yellow', textAlign: 'center' }} variant="h2">WHAT IS SOLOTTO</Typography>
 
-                        <Typography className="des" sx={{ color: 'white', textAlign: 'center', fontWeight: 600, mt: 3 }} variant="h5">
+                        <Typography className="des font-style" sx={{ color: 'white', textAlign: 'center', fontWeight: 600, mt: 3 }} variant="h5">
 
                             This is a new NFT project on the Solana blockchain. It is a 1 in 5000 chance of winning US$1Million. <br />
 
@@ -75,13 +65,13 @@ const Home = () => {
                                 <img style={{ width: "80px" }} src="https://solotto.org/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fassets%2Fimages%2Fmillionaire-wallet.90826239542d651e3025a2deff14bd88.png&w=1920&q=75" alt="" />
                             </Box>
                             <Box>
-                                <Typography className="des wallet" sx={{ typography: { md: 'h4' }, color: 'white', textAlign: 'center', mb: 3 }}>Connect Wallet</Typography>
+                                <Typography className="des wallet font-style" sx={{ typography: { md: 'h4' }, color: 'white', textAlign: 'center', mb: 3 }}>Connect Wallet</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>1. Install Phantom  (<a style={{ color: 'white', textDecoration: 'none' }} href="https://phantom.app/">https://phantom.app/</a>) wallet</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>1. Install Phantom  (<a style={{ color: 'white', textDecoration: 'none' }} href="https://phantom.app/">https://phantom.app/</a>) wallet</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>2. Transfer SOL to your wallet</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>2. Transfer SOL to your wallet</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white' }}>3. Connect wallet</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white' }}>3. Connect wallet</Typography>
 
                             </Box>
                         </Grid>
@@ -91,13 +81,13 @@ const Home = () => {
                                 <img style={{ width: "50px" }} src="https://solotto.org/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fassets%2Fimages%2Fmillionaire-hand.9b192ab4442de23cc4a0fb1339298108.png&w=1920&q=75" alt="" />
                             </Box>
                             <Box>
-                                <Typography className="des wallet" sx={{ typography: { md: 'h4' }, color: 'white', textAlign: 'center', mb: 3 }}>Mint SoLotto</Typography>
+                                <Typography className="des wallet font-style" sx={{ typography: { md: 'h4' }, color: 'white', textAlign: 'center', mb: 3 }}>Mint SoLotto</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>1. Select the amount of NFT (s) you wish to purchase and click MINT</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>1. Select the amount of NFT (s) you wish to purchase and click MINT</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>2. Click "Approve" on the popup (gas fee and other fees will be applied at your expenses)</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>2. Click "Approve" on the popup (gas fee and other fees will be applied at your expenses)</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white' }}>3. Then you will see the NFT (s) , in your wallet after a period of time</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white' }}>3. Then you will see the NFT (s) , in your wallet after a period of time</Typography>
 
                             </Box>
                         </Grid>
@@ -107,11 +97,11 @@ const Home = () => {
                                 <img style={{ width: "80px" }} src="https://solotto.org/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fassets%2Fimages%2Fmillionaire-win.348717628fab0828c6b9ae947511864c.png&w=1920&q=75" alt="" />
                             </Box>
                             <Box>
-                                <Typography className="des wallet" sx={{ typography: { md: 'h4' }, color: 'white', textAlign: 'center', mb: 3 }}>Win SoLotto</Typography>
+                                <Typography className="des wallet font-style" sx={{ typography: { md: 'h4' }, color: 'white', textAlign: 'center', mb: 3 }}>Win SoLotto</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>1. Wait for the announcement</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>1. Wait for the announcement</Typography>
 
-                                <Typography className="des how-to" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>2. You will receive a message if you win through Wallet Chat</Typography>
+                                <Typography className="des how-to font-style" sx={{ typography: { md: 'h6' }, color: 'white', mb: 3 }}>2. You will receive a message if you win through Wallet Chat</Typography>
 
                             </Box>
                         </Grid>
@@ -126,7 +116,7 @@ const Home = () => {
                         </Grid>
                         <Grid md={6} item>
                             <Typography className="soloto cost" sx={{ color: '#00f5ff' }} variant="h2">COST</Typography>
-                            <Typography className="des solona" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">2 SOL(SOLANA) per Unit.</Typography>
+                            <Typography className="des solona font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">2 SOL(SOLANA) per Unit.</Typography>
                         </Grid>
                     </Grid>
                 </Box>
@@ -135,7 +125,7 @@ const Home = () => {
 
                 <Box sx={{ mt: '100px', mb: '100px' }}>
                     <Typography className="soloto cost" sx={{ color: 'yellow' }} variant="h2">RULES</Typography>
-                    <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">
+                    <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">
                         The draw in this project is completely random. No one can manipulate it. <br /> <br />
 
                         Once the 5000 NFTs are sold out, we will broadcast the lottery live on YouTube. <br /> <br />
@@ -155,14 +145,14 @@ const Home = () => {
                 <Box sx={{ mb: '100px' }}>
                     <Typography className="soloto unit" sx={{ color: 'yellow', textAlign: 'end' }} variant="h2">NUMBERS OF UNITS</Typography>
 
-                    <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3, textAlign: 'end' }} variant="h5">5,000 units per event <br /> ※If 5,000 units are sold early, the second event will then be sold.</Typography>
+                    <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3, textAlign: 'end' }} variant="h5">5,000 units per event <br /> ※If 5,000 units are sold early, the second event will then be sold.</Typography>
                 </Box>
 
 
 
                 <Box sx={{ mt: '100px', mb: '100px' }}>
                     <Typography className="soloto" sx={{ color: 'yellow', textAlign: 'center' }} variant="h2">WHY SOLOTTO</Typography>
-                    <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">
+                    <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">
                         SoLotto will be managed on the Solana blockchain. <br /> <br />
 
                         The Solana blockchain is a system with very low risk for potential fraud <br /> and tampering. <br /> <br />
@@ -179,9 +169,9 @@ const Home = () => {
                     <Grid container spacing={2}>
                         <Grid md={6} item>
                             <Typography className="soloto cost" sx={{ color: 'yellow', }} variant="h2">MINT</Typography>
-                            <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">PRICE: 2SOL($282.26)</Typography>
-                            <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">1 SoLotto: US$289.76 worth of Solana (current rate of 2 SOL)</Typography>
-                            <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">SOL Price: live by <a style={{ color: 'white', textDecoration: 'none' }} href="https://binance.com">binance.com</a></Typography>
+                            <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">PRICE: 2SOL($282.26)</Typography>
+                            <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">1 SoLotto: US$289.76 worth of Solana (current rate of 2 SOL)</Typography>
+                            <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">SOL Price: live by <a style={{ color: 'white', textDecoration: 'none' }} href="https://binance.com">binance.com</a></Typography>
                         </Grid>
 
 
@@ -211,17 +201,18 @@ const Home = () => {
                         <Grid sx={{ mb: 10 }} spacing={1} container>
                             <Grid md={4} item>
                                 <Typography className="soloto second units" sx={{ color: 'yellow' }} variant="h4">1ST PRIZE</Typography>
-                                <Typography className="des units" sx={{ color: 'white', mt: 3 }} variant="h5">6,670SOL ($0.94Million) ×1</Typography>
+
+                                <Typography className="des units font-style" sx={{ color: 'white', mt: 3 }} variant="h5">6,670SOL ($0.94Million) ×1</Typography>
 
                             </Grid>
                             <Grid md={4} item>
                                 <Typography className="soloto second units" sx={{ color: 'yellow' }} variant="h4">2ND PRIZE</Typography>
-                                <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">66.7SOL ($9,413.37) ×9</Typography>
+                                <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">66.7SOL ($9,413.37) ×9</Typography>
 
                             </Grid>
                             <Grid md={4} item>
                                 <Typography className="soloto second units" sx={{ color: 'yellow' }} variant="h4">3RD PRIZE</Typography>
-                                <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">4SOL ($564.52) × 200</Typography>
+                                <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">4SOL ($564.52) × 200</Typography>
 
                             </Grid>
                         </Grid>
@@ -229,28 +220,31 @@ const Home = () => {
                         <Grid direction="row-reverse" spacing={2} sx={{ mb: 10 }} container>
                             <Grid md={4} item>
                                 <Typography className="soloto second units" sx={{ color: 'yellow' }} variant="h4">MILLIONAIRE MAKE</Typography>
-                                <Typography className="des units" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">66.7SOL ×1</Typography>
+                                <Typography className="des units font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">66.7SOL ×1</Typography>
                             </Grid>
                             <Grid md={4} item>
                                 <Typography className="soloto second rate-1" sx={{ color: 'yellow' }} variant="h4">RATE</Typography>
 
-                                <Typography className="des rate-2" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">1SOL($141.13)</Typography>
+                                <Typography className="des rate-2 font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">1SOL($141.13)</Typography>
                             </Grid>
                             <Grid md={4} item>
-                                <Typography className="des rate-3" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">※Mint = Purchase</Typography>
+                                <Typography className="des rate-3 font-style" sx={{ color: 'white', fontWeight: 600, mt: 3 }} variant="h5">※Mint = Purchase</Typography>
                             </Grid>
                         </Grid>
 
                     </Box>
                 </Box>
-
-
-                <footer>
-                    <Typography sx={{ color: 'white', fontWeight: 600, textAlign: 'center', pb: 3 }} variant="h5">©2021 SOLOTTO. All rights reserved.</Typography>
-                </footer>
-
-
             </Container>
+
+            <div className='social-media'>
+                <i className="fab fa-twitter"></i>
+                <i className="fab fa-discord"></i>
+            </div>
+
+            <footer>
+                <Typography className='font-style' sx={{ color: 'white', fontWeight: 600, textAlign: 'center', pb: 3 }} variant="h5">©2021 SOLOTTO. All rights reserved.</Typography>
+            </footer>
+
         </div>
     );
 };
